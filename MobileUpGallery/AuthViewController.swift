48c0b24953a492060f9b8dc986e982c1f22419bf
Olesia <odobrogor@gmail.com>
2021-07-20 18:58:10 +0300

    AuthViewController (new main view controller), AuthService

---
+++ MobileUpGallery/AuthViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MobileUpGallery
+//
+//  Created by Олеся Егорова on 20.07.2021.
+//
+
+import UIKit
+import VK_ios_sdk
+
+
+class AuthViewController: UIViewController {
+    
+    private var authService: AuthService!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        authService = AuthService()
+        
+    }
+    @IBAction func enterButton(_ sender: UIButton) {
+        authService.wakeUpSession()
+    }
+    
+
+}
+
